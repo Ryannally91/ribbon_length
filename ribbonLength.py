0ef927333161a6f74077fb3dd62a6f271f8cd6dd
Ryan Nally <ryannally91@gmail.com>
2022-01-21 10:43:03 -0600

    removed task description

--- ribbonLength.py
+++ ribbonLength.py
@@ -14,10 +14,4 @@ def solution(a, k):
 print(solution([4, 4, 7, 1, 4, 9, 5, 5, 8, 4, 1, 1], 2))
 
 
-#RIBBON cutting
 
-# You are given an array of integers a, where each element a[i] represents the length of a ribbon.
-# Your goal is to obtain k ribbons of the same length, by cutting the ribbons into as many pieces as you want.
-# Your task is to calculate the maximum integer length L for which it is possible to obtain at least k ribbons of length L by cutting the given ones.
-# Example
-# For a = [5, 2, 7, 4, 9] and k = 5, the output should be solution(a, k) = 4.
